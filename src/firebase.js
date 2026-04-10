6cc4453e1378c7e47db7168d98dda16929153794
CuriosVoult <curiosvoult.help@gmail.com>
2026-04-10 09:24:54 -0500

    Enhance Firebase auth with email/password methods
    Added email/password authentication methods and removed unused imports.

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,14 @@
-// Importaciones usando links oficiales de Firebase (SDK v10+)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getFirestore, doc, setDoc, getDoc, updateDoc, increment } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
+import { 
+  getAuth, 
+  GoogleAuthProvider, 
+  signInWithPopup, 
+  signOut, 
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword 
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyAR4AkM8MaQBqA1MDUyQf5UeCqSntdiz70",
@@ -10,15 +16,21 @@ const firebaseConfig = {
     projectId: "codeshelf-c",
     storageBucket: "codeshelf-c.firebasestorage.app",
     messagingSenderId: "688503183462",
-    appId: "1:688503183462:web:a2f3e945dc7b8bbca0b5a7",
-    measurementId: "G-N0NP15BMKB"
+    appId: "1:688503183462:web:a2f3e945dc7b8bbca0b5a7"
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
 
-export { signInWithPopup, signOut, onAuthStateChanged, doc, setDoc, getDoc, updateDoc, increment };
+// Exportamos las nuevas funciones
+export { 
+  signInWithPopup, 
+  signOut, 
+  onAuthStateChanged, 
+  signInWithEmailAndPassword, 
+  createUserWithEmailAndPassword,
+  doc, 
+  setDoc 
+};
