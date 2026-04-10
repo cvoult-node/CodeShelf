bde6e4e4ac80f3f9f5bf6cf25baff5752c10f6ab
CuriosVoult <curiosvoult.help@gmail.com>
2026-04-10 09:09:56 -0500

    Add Firebase SDK initialization and configuration

---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+// Importaciones usando links oficiales de Firebase (SDK v10+)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore, doc, setDoc, getDoc, updateDoc, increment } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAR4AkM8MaQBqA1MDUyQf5UeCqSntdiz70",
+    authDomain: "codeshelf-c.firebaseapp.com",
+    projectId: "codeshelf-c",
+    storageBucket: "codeshelf-c.firebasestorage.app",
+    messagingSenderId: "688503183462",
+    appId: "1:688503183462:web:a2f3e945dc7b8bbca0b5a7",
+    measurementId: "G-N0NP15BMKB"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
+
+export { signInWithPopup, signOut, onAuthStateChanged, doc, setDoc, getDoc, updateDoc, increment };
